3397f5235a7fd1a578d28a019916d98e7b1b7a75
创圣大天使Evol <296662402@qq.com>
2024-09-30 20:41:56 +0800

    update config of prettier and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,20 @@
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import prettier from 'eslint-plugin-prettier';
-import jest from 'eslint-plugin-jest';
-import globals from 'globals';
-import tsParser from '@typescript-eslint/parser';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import js from '@eslint/js';
-import { FlatCompat } from '@eslint/eslintrc';
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import prettier from 'eslint-plugin-prettier'
+import jest from 'eslint-plugin-jest'
+import globals from 'globals'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: js.configs.recommended,
   allConfig: js.configs.all,
-});
+})
 
 export default [
   {
@@ -43,8 +43,8 @@ export default [
     },
 
     rules: {
-      'no-console': 1,
+      'no-console': 'off',
       'prettier/prettier': 2,
     },
   },
-];
+]
--- src/useCase/example.ts
+++ src/useCase/example.ts
@@ -1,18 +1,18 @@
 export class Guesser {
-  private word = '';
-  private correctGuesses = 0;
+  private word = ''
+  private correctGuesses = 0
 
   public setWord(word: string) {
-    this.word = word;
+    this.word = word
   }
 
   public guessWord(word: string): boolean {
-    const correct = word.toLowerCase() === this.word.toLowerCase();
-    if (correct) this.correctGuesses++;
-    return correct;
+    const correct = word.toLowerCase() === this.word.toLowerCase()
+    if (correct) this.correctGuesses++
+    return correct
   }
 
   public getNumberCorrectGuesses(): number {
-    return this.correctGuesses;
+    return this.correctGuesses
   }
 }
